473914a6104cc1d8eee416b25d51759150148a1a
khoadino <haruyuki7121994@gmail.com>
2026-09-06 15:41:26 +0700

    init app

---
+++ src/main/java/com/learning/learningtransactional/LearningTransactionalApplication.java
@@ -0,0 +1,13 @@
+package com.learning.learningtransactional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningTransactionalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningTransactionalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/learning/learningtransactional/LearningTransactionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.learningtransactional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningTransactionalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
